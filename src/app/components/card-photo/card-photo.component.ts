00c715c7eb5a84912056e94a4493a914b6caf4c6
XeilaS <Alexis.nancelle@ecole-it.com>
2023-07-31 20:29:58 +0200

    feat: added filter to dataCard Page , fix somes errors and added a section to the homePage for bug reporting

--- src/app/components/card-photo/card-photo.component.ts
+++ src/app/components/card-photo/card-photo.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Photo } from 'src/app/models/photoModels';
 import { ListPhotoService } from 'src/app/services/list-photo-service/list-photo-service';
-import { ImageModalComponent } from '../image-modal/image-modal.component';
+import { Photo } from 'src/app/models/photoModels';
 import { MatDialog } from '@angular/material/dialog';
+import { ImageModalComponent } from 'src/app/components/image-modal/image-modal.component';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
@@ -13,11 +13,13 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator';
 export class CardPhotoComponent implements OnInit {
   photos: Photo[] = [];
   totalItems = 0;
-  pageSize= 10;
+  pageSize = 10;
   pageSizeOptions: number[] = [10, 15, 25];
   visiblePhotos: Photo[] = [];
+  currentFilterValue = '';
 
-  constructor(private listPhotoService: ListPhotoService, public dialog: MatDialog) { }
+  constructor(private listPhotoService: ListPhotoService,public dialog: MatDialog) { }
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngOnInit() {
@@ -40,5 +42,30 @@ export class CardPhotoComponent implements OnInit {
       data: { imageUrl },
       panelClass: 'custom-dialog-container'
     });
+  }
+
+  applyFilter(event: Event, property: string) {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.currentFilterValue = filterValue;
+    
+    if (property === 'id') {
+      const filterId = parseInt(filterValue, 10);
+      if (isNaN(filterId)) {
+        // Si le filtre ID n'est pas un nombre valide, réinitialiser la liste visiblePhotos à la liste complète des photos
+        this.visiblePhotos = this.photos;
+      } else {
+        this.visiblePhotos = this.photos.filter(photo => photo.id === filterId);
+      }
+    } else if (property === 'title') {
+      this.visiblePhotos = this.photos.filter(photo => photo.title.toLowerCase().includes(filterValue));
+    }
+  
+    if (this.visiblePhotos.length === 0) {
+      const noDataMessage = `Rien n'a été trouvé avec le filtre "${filterValue}"`;
+      this.visiblePhotos.push({ albumId: -1, id: -1, title: noDataMessage, url: '', thumbnailUrl: '' });
+    }
+  
+    // Reset pagination
+    this.paginator.firstPage();
   }
 }
